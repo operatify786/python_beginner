ede42d5e085bc89b8eda18d07d1139a94c099b93
operatify786 <operatify@gmail.com>
2026-01-03 12:30:36 +0500

    Add files via upload

---
+++ 12_sleep.py
@@ -0,0 +1,15 @@
+import time
+
+for x in reversed(range(1, 11)):
+    print(x)
+    time.sleep(0.5)
+print("Happy New Year 2026")
+
+
+username = "abc";
+password = "123";
+if (username == "abc" and password == "123"):
+    time.sleep(1)
+    print("Welcome")
+else:
+    print("Wrong username or password")
